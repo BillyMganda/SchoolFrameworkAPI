55b47a88b5b41bf0597f552fe462669be9f7007b
Billy <bimganda@gmail.com>
2024-08-13 21:08:34 +0300

    Create a unity DI container to handle all DI instances

---
+++ SchoolFrameworkAPI/App_Start/UnityConfig.cs
@@ -0,0 +1,24 @@
+﻿using SchoolFrameworkAPI.Controllers;
+using SchoolFrameworkAPI.Repositories;
+using System.Web.Http;
+using Unity;
+using Unity.Lifetime;
+
+namespace SchoolFrameworkAPI.App_Start
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            // Register repositories and other dependencies here
+            container.RegisterType<IDepartmentRepository, DepartmentRepository>(new HierarchicalLifetimeManager());
+
+            // Register controllers if necessary
+            //container.RegisterType<DepartmentsController>();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);
+        }
+    }
+}

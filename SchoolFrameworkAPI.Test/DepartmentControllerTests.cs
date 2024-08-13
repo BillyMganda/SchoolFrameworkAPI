efe717c28b8040261fc6fbd3b19eed4a0e97d231
Billy <bimganda@gmail.com>
2024-08-13 12:00:35 +0300

    Add test project with a method to test get all departments of departments controller.

---
+++ SchoolFrameworkAPI.Test/DepartmentControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SchoolFrameworkAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SchoolFrameworkAPI.Test
+{
+    [TestClass]
+    public class DepartmentControllerTests
+    {             
+        [TestMethod]
+        public void GetDepartments_ShouldReturnAllDepartments()
+        {
+            // Arrange
+            var data = new List<Department>
+            {
+                new Department { Id = 1, Name = "HR", DateCreated = DateTime.UtcNow },
+                new Department { Id = 2, Name = "IT", DateCreated = DateTime.UtcNow }
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Department>>();
+            mockSet.As<IQueryable<Department>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Department>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Department>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Department>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<ScoolFrameworkEntities>();
+            mockContext.Setup(c => c.Department).Returns(mockSet.Object);
+
+            // Act
+            var controller = new DepartmentsController();
+            var result = controller.GetDepartments();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("HR", result.ElementAt(0).Name);
+            Assert.AreEqual("IT", result.ElementAt(1).Name);
+        }
+    }
+}
---
+++ SchoolFrameworkAPI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchoolFrameworkAPI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchoolFrameworkAPI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ab89bad-df2e-4c4b-be52-a4e51974d404")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

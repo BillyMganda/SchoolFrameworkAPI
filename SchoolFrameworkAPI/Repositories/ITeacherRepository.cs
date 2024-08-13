0809e3e7a8b1964afa27d0b5bf52ee6d60d91154
Billy <bimganda@gmail.com>
2024-08-13 22:26:13 +0300

    add interface class that manages a teacher

---
+++ SchoolFrameworkAPI/Repositories/ITeacherRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer;
+using SchoolFrameworkAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SchoolFrameworkAPI.Repositories
+{
+    public interface ITeacherRepository
+    {
+        Task<IEnumerable<Teacher>> GetTeachersAsync();
+        Task<Teacher> GetTeacherByIdAsync(int id);
+        Task CreateTeacherAsync(CreateTeacherRequest request);
+        Task UpdateTeacherAsync(UpdateTeacherRequest request);
+        Task DeleteTeacherAsync(int id);
+    }
+}

b67a709155647a895589bd2fda6ec5f614c48ca1
Billy <bimganda@gmail.com>
2024-08-19 11:15:36 +0300

    add student dto classes

---
+++ SchoolFrameworkAPI/Models/GetStudentResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace SchoolFrameworkAPI.Models
+{
+    public class GetStudentResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string ParentOrGuardianFirstName { get; set; }
+        public string ParentOrGuardianLastName { get; set; }
+        public string ParentOrGuardianPhoneNumber { get; set; }
+        public string ParentOrGuardianEmailAddress { get; set; }
+        public string FormName { get; set; }
+    }
+}
---
+++ SchoolFrameworkAPI/Models/UpdateStudentRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolFrameworkAPI.Models
+{
+    public class UpdateStudentRequest
+    {
+        public int Id { get; set; }        
+        public string ParentOrGuardianFirstName { get; set; }
+        public string ParentOrGuardianLastName { get; set; }
+        public string ParentOrGuardianPhoneNumber { get; set; }
+        public string ParentOrGuardianEmailAddress { get; set; }
+        public int FormId { get; set; }
+    }
+}
